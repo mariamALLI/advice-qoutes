97962e3f48fa2bdcb2a074582cfa4f50cc46fbdf
Mariam Oluseyi-Alli <81536650+mariamALLI@users.noreply.github.com>
2024-10-22 11:56:26 +0100

    improve desktop version
    improved functionality and active state

--- main.js
+++ main.js
@@ -1,5 +1,9 @@
 const adviceBox = document.querySelector('#advice_box');
 const adviceQuoteId = document.querySelector('#advice_quote_id')
+const fetchBtn = document.querySelector('#fetch-btn')
+const clickFetch = document.querySelector('#click-fetch')
+
+
 const url = 'https://api.adviceslip.com/advice';
 
 const getAdvice = async () => {
@@ -9,20 +13,21 @@ const getAdvice = async () => {
             throw new Error('Failed to fetch advice');
         }else{
             const data = await response.json();
-            console.log(data)
             const adviceId = data.slip.id
             const adviceQuote = data.slip.advice
             adviceBox.innerHTML = `
-            <div class="text-neon_green font-bold font-manrope text-center text-sm my-3" id="advice_quote_id">
+            <div class="text-neon_green font-bold font-manrope text-sm my-3" id="advice_quote_id">
                 ADVICE #${adviceId}
             </div>
 
-            <div class="my-4 text-gray-300 text-xl text-center font-bold font-manrope" id="advice_quotes">
+            <div class="my-4 text-light_cyan text-xl text-center font-bold font-manrope" id="advice_quotes">
             &#34${adviceQuote}&#34
             </div>
 
             <div class="my-8 p-4">
-                <img src="images/pattern-divider-mobile.svg" alt="advice-line-img">
+                <img class="sm:hidden" src="images/pattern-divider-mobile.svg" alt="advice-line-img">
+                <img class="hidden sm:block" src="images/pattern-divider-desktop.svg" alt="advice-line-img">
+
             </div>
 
             `
@@ -32,3 +37,8 @@ const getAdvice = async () => {
     }
 }
 getAdvice()
+
+fetchBtn.addEventListener('click', (event)=> {
+    console.log(event.target, 'click')
+    return getAdvice()
+})
